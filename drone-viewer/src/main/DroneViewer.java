5e4642f0e30092b2b1522124349aa2ead42125ab
TimoLehnertz <timolehnertz1@gmail.com>
2022-04-13 13:07:13 +0200

    2

--- drone-viewer/src/main/DroneViewer.java
+++ drone-viewer/src/main/DroneViewer.java
@@ -1,12 +1,13 @@
 package main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -132,6 +133,14 @@ public class DroneViewer extends XIdePreset {
 				GuiLogic.getInstance().getSerialInterface().selectComPort(comboMap.get((String) selectCombo.getSelectedItem()));
 			}
 		});
+		new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 		disconnectButton.addActionListener(e -> serial.disconnectSerial());
 		refreshButton.addActionListener(e -> refrechSerial());
 	}
